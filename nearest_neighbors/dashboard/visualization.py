c97a64e7f2a4fdff62fd59e7b7fab5ffbcc7d1f3
Denis Newman-Griffis <dnewmangriffis@pitt.edu>
2021-01-04 18:54:15 -0500

    Styling updates

--- nearest_neighbors/dashboard/visualization.py
+++ nearest_neighbors/dashboard/visualization.py
@@ -61,11 +61,11 @@ def pairwiseSimilarityAnalysis(corpora, means, stds, outf=None, figsize=(5,5), f
 
     (fig, ax) = plt.subplots(figsize=figsize)
 
-    x_ticks = np.arange(len(corpora)) + 1
+    x_ticks = np.arange(len(corpora))
     x_data, y_data, y_err = [], [], []
     for i in range(len(corpora)):
         if not (means[i] is None):
-            x_data.append(i+1)
+            x_data.append(i)
             y_data.append(means[i])
             y_err.append(stds[i])
 
@@ -83,11 +83,15 @@ def pairwiseSimilarityAnalysis(corpora, means, stds, outf=None, figsize=(5,5), f
         fmt='r--'
     )
 
-    plt.xlabel('Corpus versions')
-    plt.ylabel('Agg. Cos. Sim.')
+    plt.xlabel('Corpus versions', labelpad=15)
+    plt.ylabel('Agg. Cos. Sim.', labelpad=10)
 
     ax.set_xticks(x_ticks)
     ax.set_xticklabels(corpora)
+    for tick in ax.xaxis.get_majorticklabels():
+        tick.set_horizontalalignment('left')
+
+    plt.xticks(rotation=-60)
 
     #for lim in [0.25, 0.5, 0.75, 1.0]:
     #    if lim > max(y_data):
@@ -113,17 +117,17 @@ def internalConfidenceAnalysis(corpora, confidences, threshold, outf=None, figsi
 
     (fig, ax) = plt.subplots(figsize=figsize)
 
-    x_ticks = np.arange(len(corpora)) + 1
+    x_ticks = np.arange(len(corpora))
     x_data, y_data = [], []
     for i in range(len(corpora)):
         if not (confidences[i] is None):
-            x_data.append(i+1)
+            x_data.append(i)
             y_data.append(confidences[i])
 
     # plot the threshold
     plt.plot(
-        np.arange(len(corpora) + 1),
-        [threshold for _ in range(len(corpora) + 1)],
+        np.arange(len(corpora)),
+        [threshold for _ in range(len(corpora))],
         '--',
         color='#dddddd'
     )
@@ -140,12 +144,15 @@ def internalConfidenceAnalysis(corpora, confidences, threshold, outf=None, figsi
         'b.'
     )
 
-    plt.xlabel('Corpus versions')
-    plt.ylabel('Confidence')
+    plt.xlabel('Corpus versions', labelpad=15)
+    plt.ylabel('Confidence', labelpad=10)
 
     ax.set_xticks(x_ticks)
     ax.set_xticklabels(corpora)
+    for tick in ax.xaxis.get_majorticklabels():
+        tick.set_horizontalalignment('left')
 
+    plt.xticks(rotation=-60)
     plt.ylim((0, 1))
 
     plt.savefig(
