e753e1be41fba27fc356220a87345d1c39fb1237
Denis Newman-Griffis <dnewmangriffis@pitt.edu>
2022-02-21 23:33:45 -0500

    Added terminology status reporting to dashboard_reporting.py

--- textessence/utils/dashboard_reporting.py
+++ textessence/utils/dashboard_reporting.py
@@ -3,9 +3,10 @@ import configparser
 from collections import OrderedDict
 from hedgepig_logger import log
 from textessence.lib import normalization
-from textessence.corpus.extract_cord19_articles import CORD19ExtractionReport
+from textessence.corpus.cord19.extract_cord19_articles import CORD19ExtractionReport
 from textessence.corpus.snapshot_data_models import *
 from textessence.corpus.compile_snapshot_corpus import CompilationData
+from textessence.terminology.terminology_data_models import *
 
 class SourceCorporaReport:
     def __init__(self, sources_config):
@@ -44,8 +45,82 @@ class SourceCorpusStatus:
 
 
 class TerminologiesReport:
-    def __init__(self):
-        pass
+    def __init__(self, base_config, terminologies_config):
+        normalization_options = normalization.loadConfiguration(base_config['Normalization'])
+
+        terminologies_root_dir = terminologies_config['Default']['RootDirectory']
+        self.collection = TerminologyCollection(
+            terminologies_root_dir
+        )
+
+        self.terminology_statuses = OrderedDict()
+        for terminology in terminologies_config['Default']['TerminologiesInUse'].split(','):
+            if terminology in terminologies_config:
+                self.terminology_statuses[terminology] = TerminologyStatus(
+                    self.collection,
+                    terminology,
+                    terminologies_config,
+                    normalization_options
+                )
+                terminology_config = terminologies_config[terminology]
+            else:
+                log.writeln('[WARNING] TerminologiesInUse setting includes terminology "{0}",'
+                            ' which was not found in the config file'.format(terminology))
+
+    def renderToLog(self):
+        log.writeln('=======================================================')
+        log.writeln('==               Terminologies Report                ==')
+        log.writeln('=======================================================')
+
+        max_terminology_len = max([len(t) for t in self.terminology_statuses.keys()])
+        fmt = lambda term, extr, cmap, filt, prep, comp: (
+            '{0:<%d} | {1:<9} | {2:<9} | {3:<8} | {4:<12} | {5:<8}' % (max_terminology_len+1)
+        ).format(term, extr, cmap, filt, prep, comp)
+
+        log.writeln(fmt(
+            '', 'Extracted', 'CatMapped', 'Filtered', 'Preprocessed', 'Compiled'
+        ))
+        for (terminology, status) in self.terminology_statuses.items():
+            log.writeln(fmt(
+                terminology,
+                'X' if status.extracted else '',
+                'X' if status.categories_mapped else '',
+                'X' if status.filtered else '',
+                'X' if status.preprocessed else '',
+                'X' if status.compiled else ''
+            ))
+
+
+class TerminologyStatus:
+    def __init__(self, collection, terminology, terminologies_config, normalization_options):
+        self.terminology = terminology
+        self.terminology_config = terminologies_config[self.terminology]
+
+        terminology_data = collection[self.terminology]
+
+        self.extracted = (
+            os.path.exists(terminology_data.raw_terminology_file)
+        )
+
+        self.categories_mapped = (
+            os.path.exists(terminology_data.category_map_file)
+        )
+
+        self.filtered = (
+            os.path.exists(terminology_data.filtered_terminology_file)
+        )
+
+        self.preprocessed = (
+            os.path.exists(
+                terminology_data.preprocessed_terminology_file(normalization_options)
+            )
+        )
+
+        self.compiled = (
+            os.path.exists(
+                terminology_data.compiled_preprocessed_terminology_file(normalization_options)
+            )
+        )
 
 
 class SnapshotCorporaReport:
@@ -129,14 +204,17 @@ class EmbeddingAnalysisReport:
 
 
 class DashboardMasterReport:
-    def __init__(self, base_config, cord19_config, snapshot_config):
+    def __init__(self, base_config, cord19_config, snapshot_config, terminology_config):
         self.source_corpora_report = SourceCorporaReport(cord19_config)
         self.snapshot_corpora_report = SnapshotCorporaReport(base_config, snapshot_config)
+        self.terminologies_report = TerminologiesReport(base_config, terminology_config)
 
     def renderToLog(self):
         self.source_corpora_report.renderToLog()
         log.writeln('\n\n')
         self.snapshot_corpora_report.renderToLog()
+        log.writeln('\n\n')
+        self.terminologies_report.renderToLog()
 
 
 
@@ -162,13 +240,18 @@ if __name__ == '__main__':
     snapshot_config = configparser.ConfigParser()
     snapshot_config.read(base_config['General']['SnapshotConfig'])
 
+    terminology_config = configparser.ConfigParser()
+    terminology_config.read(base_config['General']['TerminologyConfig'])
+
     logfile = base_config['Logging']['OverviewReport']
     log.start(logfile)
     log.writeConfig([
-        ('Base configuration file', options.config_f)
+        ('Base configuration file', options.config_f),
+        ('Snapshot configuration file', base_config['General']['SnapshotConfig']),
+        ('Terminology configuration file', base_config['General']['TerminologyConfig']),
     ], 'Overview Dashboard Report')
 
-    report = DashboardMasterReport(base_config, cord19_config, snapshot_config)
+    report = DashboardMasterReport(base_config, cord19_config, snapshot_config, terminology_config)
     report.renderToLog()
 
     log.stop()
